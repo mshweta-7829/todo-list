197b680cadb0d96e2a1cd3601c96410bd93123ba
Shweta <arunbasappagudasi@Aruns-MacBook-Pro.local>
2020-10-11 14:18:53 -0700

    Updates

---
+++ precache-manifest.df2ecab15b42448b1bf624eac86d8884.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e7e3dcd935b6b38c6116842de8d0c47",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "16df72572efc3aa2c280",
+    "url": "/todo-list/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "deab3aec08a77b7623a2",
+    "url": "/todo-list/static/css/main.2dd59b60.chunk.css"
+  },
+  {
+    "revision": "16df72572efc3aa2c280",
+    "url": "/todo-list/static/js/2.2a610cff.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-list/static/js/2.2a610cff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "deab3aec08a77b7623a2",
+    "url": "/todo-list/static/js/main.910ad940.chunk.js"
+  },
+  {
+    "revision": "3646663ad3aa9c4dda3a",
+    "url": "/todo-list/static/js/runtime-main.74693e7a.js"
+  }
+]);
